a843fe9b864ae0297bc502495b42ba75fa2ee400
nischit <nischitpra@gmail.com>
2023-09-12 00:29:16 +0545

    deployment script

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+current_branch=$(git branch | grep "*")
+if [ "$current_branch" != "* master" ]; then
+    echo "Not in master"
+    exit 1
+fi
+
+echo "In $current_branch"
+
+npm version patch
+
+mv .git* ..
+mv node_modules ..
+mv .vscode ..
+cd ..
+tar cvzf MercleIpfsRpcGateway.tar MercleIpfsRpcGateway/
+scp -i "~/.ssh/daohook-prod.pem" MercleIpfsRpcGateway.tar ec2-user@ec2-3-88-169-14.compute-1.amazonaws.com:/home/ec2-user/
+
+mv .git* MercleIpfsRpcGateway
+mv node_modules MercleIpfsRpcGateway
+mv .vscode MercleIpfsRpcGateway
+
+ssh_run_server() {
+    echo "ssh -i " $1 $2
+ssh -i $1 $2 << EOF
+  rm -rf _MercleIpfsRpcGateway
+  mv MercleIpfsRpcGateway _MercleIpfsRpcGateway
+  tar xvzf MercleIpfsRpcGateway.tar
+  cp _MercleIpfsRpcGateway/.env* MercleIpfsRpcGateway/
+  rm MercleIpfsRpcGateway.tar
+  cd MercleIpfsRpcGateway
+  npm i
+  cd ..
+  cd MercleIpfsRpcGateway
+  pm2 reload all
+EOF
+}
+
+ssh_run_server "~/.ssh/daohook-prod.pem" ec2-user@ec2-3-88-169-14.compute-1.amazonaws.com
